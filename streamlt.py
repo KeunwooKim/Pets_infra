053050b1458354d61d82bf6728855fa6d6d59227
KeunwooKim <70514097+KeunwooKim@users.noreply.github.com>
2024-12-02 15:16:46 +0900

    Update streamlt.py

--- streamlt.py
+++ streamlt.py
@@ -6,11 +6,11 @@ import streamlit as st
 import colorsys
 
 # GeoJSON 파일 로드 및 GeoDataFrame으로 변환
-seoul_geo_path = 'hangjeongdong_서울특별시.geojson'
+seoul_geo_path = 'resource/hangjeongdong_서울특별시.geojson'
 seoul_gdf = gpd.read_file(seoul_geo_path)
 
 # 시설 데이터 로드
-facilities_df = pd.read_csv('서울반려동물동반.csv', encoding='utf-8')
+facilities_df = pd.read_csv('resource/서울반려동물동반.csv', encoding='utf-8')
 
 # 구별로 병합
 seoul_gu_gdf = seoul_gdf.dissolve(by='sggnm')
