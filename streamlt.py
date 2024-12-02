0b36a46b84621311f143558002edacc3287fa37f
KeunwooKim <70514097+KeunwooKim@users.noreply.github.com>
2024-12-02 16:17:31 +0900

    Update streamlt.py

--- streamlt.py
+++ streamlt.py
@@ -7,7 +7,11 @@ import colorsys
 
 # GeoJSON 파일 로드 및 GeoDataFrame으로 변환
 seoul_geo_path = 'resource/hangjeongdong_서울특별시.geojson'
-seoul_gdf = gpd.read_file(seoul_geo_path)    
+try:
+    seoul_gdf = gpd.read_file(seoul_geo_path)
+except Exception as e:
+    print(f"Error reading GeoJSON file: {e}")
+
 
 # 시설 데이터 로드
 facilities_df = pd.read_csv('resource/서울반려동물동반.csv', encoding='utf-8')
